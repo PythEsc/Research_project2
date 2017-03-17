e92e82fc1520f8c6261d3a6a6d19ea427828a6e8
florian557p <floriankrebs.krebs@student.maastrichtuniversity.nl>
2017-03-17 16:32:30 +0100

    Some code adjustment for word2vec: - Added an idx_word_map that maps from a given index to the corresponding word - Added comments - Added code that checks if vectorised data-set file does already exist - Added the beginning of the code that will be used for graphical vectorising

--- Programming/Importer/Word2vec.py
+++ Programming/Importer/Word2vec.py
@@ -1,35 +1,46 @@
-import re
-from Programming.Importer.DataImporter import DataImporter
+import matplotlib.pyplot as plt
 import nltk
+import numpy as np
+import os
+import pickle
+import re
 import string as stringlib
 from collections import defaultdict
-import numpy as np
-import pickle
+from Programming.Importer.DataImporter import DataImporter
+from sklearn.manifold import TSNE
 
 
-def load_bin_vec(fname, vocab):
-    """
-    Loads 300x1 word vecs from Google (Mikolov) word2vec
-    """
+def load_bin_vec(filename, vocabulary):
     word_vecs = {}
-    with open(fname, "rb") as f:
+    # Open the file <filename> as binary
+    with open(filename, "rb") as f:
+        # Read the first line that contains the header
         header = f.readline()
+        # Split the header and get the size information
         vocab_size, layer1_size = map(int, header.split())
+        # The length of <layer1_size> 32bit floats
         binary_len = np.dtype('float32').itemsize * layer1_size
+
         for line in range(vocab_size):
+            # Create an interactive loading bar that displays the current status of processing the file
             percent = "{0:.1f}".format(100 * (line / float(vocab_size)))
             filled_length = int(100 * line // vocab_size)
             bar = '#' * filled_length + '-' * (100 - filled_length)
             print('\r%s |%s| %s%%' % ('\t', bar, percent), end='')
+
             word = []
+            # Read bytes as long as there is no space
             while True:
+                # The binary file is encoded as ISO-8859-1
                 ch = f.read(1).decode("ISO-8859-1")
+                # If we read a blank character the word is finished -> We join all the characters to a string and break
                 if ch == ' ':
                     word = ''.join(word)
                     break
                 if ch != '\n':
                     word.append(ch)
-            if word in vocab:
+            # If the word is in our vocabulary we will add it to our word_vector
+            if word in vocabulary:
                 word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
             else:
                 f.read(binary_len)
@@ -38,26 +49,27 @@ def load_bin_vec(fname, vocab):
 
 
 def get_w(word_vecs, k=300):
-    """
-    Get word matrix. w[i] is the vector for word indexed by i
-    """
     vocab_size = len(word_vecs)
     word_idx_map = dict()
-    w = np.zeros(shape=(vocab_size+1, k), dtype='float32')
+    idx_word_map = dict()
+
+    w = np.zeros(shape=(vocab_size + 1, k), dtype='float32')
     w[0] = np.zeros(k, dtype='float32')
+
     i = 1
+    # Iterate over the keys (so the words) of our vocabulary
     for word in word_vecs:
+        # Add each word's numerical representation to w
         w[i] = word_vecs[word]
+        # Add the entry to the maps that helps us finding entries in w on later usage
         word_idx_map[word] = i
+        idx_word_map[i] = word
+
         i += 1
-    return w, word_idx_map
+    return w, word_idx_map, idx_word_map
 
 
 def clean_str(string):
-    """
-    Tokenization/string cleaning for all datasets except for SST.
-    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
-    """
     string = string.lower()
     string = re.sub(r"[^A-Za-z0-9(),!?\'`]", " ", string)
     # Instead of using those lines we are replacing every "'" with " " such that words like "I'm" -> "I m" and get
@@ -76,9 +88,9 @@ def clean_str(string):
     string = re.sub(r"\(", " ( ", string)
     string = re.sub(r"\)", " ) ", string)
     string = re.sub(r"\?", " ? ", string)
-    # Convert www.* or https?://* to URL
+    # Convert www.* or https?://* to __URL__
     string = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '__URL__', string)
-    # Convert @username to AT_USER
+    # Convert @username to __AT_USER__
     string = re.sub('@[^\s]+', '__AT_USER__', string)
     # Remove additional white spaces
     string = re.sub('[\s]+', ' ', string)
@@ -88,50 +100,99 @@ def clean_str(string):
     string = re.sub(r'(.)\1{2,}', r'\1', string)
     # Remove words with 2 or less characters
     string = re.sub(r'\b\w{1,2}\b', '', string)
+    # Remove sequences that contain numbers
+    string = re.sub(r'\b\w*\d\w*\b', '', string)
     # trim
     return string.strip('\'"').strip()
 
 
+def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
+  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+  plt.figure(figsize=(18, 18))  # in inches
+  for i, label in enumerate(labels):
+    x, y = low_dim_embs[i, :]
+    plt.scatter(x, y)
+    plt.annotate(label,
+                 xy=(x, y),
+                 xytext=(5, 2),
+                 textcoords='offset points',
+                 ha='right',
+                 va='bottom')
+
+  plt.savefig(filename)
+
+# Create our DataImporter for the Sainsbury data set
 importer_sainsbury = DataImporter("../Filtered/Sainsbury.zip",
                                   "../Unzipped/Sainsbury")
-
+# Load the data
 importer_sainsbury.load()
 
-print("---------- Start downloading stopwords ----------")
-nltk.download("stopwords")
-print("---------- Finished downloading stopwords ----------\n")
+# If the vectorised data set already exists and could be loaded from file ask user if he wants to load it or
+# overwrite it
+if os.path.exists("../Model/dataset.p"):
+    input_string = input("There is an already existing vectorised data set. Do you want to load (l) it or overwrite ("
+                         "o) it?")
 
-punctuation = list(stringlib.punctuation)
+    while input_string != "l" and input_string != "o":
+        input_string = input("Please either overwrite the existing file by writing 'o' or load it by writing 'l'")
 
-stop = nltk.corpus.stopwords.words('english') + punctuation
-stop.append('__AT_USER__')
-stop.append('__URL__')
+    if input_string == "o":
+        print("---------- Start downloading stopwords ----------")
+        nltk.download("stopwords")
+        print("---------- Finished downloading stopwords ----------\n")
 
-print("---------- Start tokenizing data ----------")
-vocab = defaultdict(float)
-for _, dataMap in importer_sainsbury.data_storage.items():
-    for comment_structure in dataMap["comments"]:
-        original_comment = comment_structure[8]
-        if not isinstance(original_comment, str):
-            continue
+        punctuation = list(stringlib.punctuation)
 
-        cleaned_comment = clean_str(original_comment)
-        cleaned_words = cleaned_comment.split()
+        stop = nltk.corpus.stopwords.words('english') + punctuation
+        stop.append('__AT_USER__')
+        stop.append('__URL__')
 
-        tokens = [word for word in cleaned_words if word not in stop]
+        print("---------- Start tokenizing data ----------")
+        vocab = defaultdict(float)
+        for _, dataMap in importer_sainsbury.data_storage.items():
+            for comment_structure in dataMap["comments"]:
+                original_comment = comment_structure[8]
+                if not isinstance(original_comment, str):
+                    continue
 
-        for word in set(tokens):
-            vocab[word] += 1
+                cleaned_comment = clean_str(original_comment)
+                cleaned_words = cleaned_comment.split()
 
-print("---------- Finished tokenizing data ----------\n")
+                tokens = [word for word in cleaned_words if word not in stop]
 
-print("---------- Start loading pre-trained word2vec model ----------")
-dictionary = load_bin_vec("../Model/GoogleNews-vectors-negative300.bin", vocab)
-print("---------- Finished loading pre-trained word2vec model ----------\n")
+                for word in set(tokens):
+                    vocab[word] += 1
 
-print("---------- Start saving vectors to file ----------")
-w, word_idx_map = get_w(dictionary)
-pickle.dump([w, word_idx_map, vocab], open("../Model/dataset.p", "wb"))
-print("---------- Finished saving vectors to file ----------\n")
+        print("---------- Finished tokenizing data ----------\n")
 
-print("-------------------- Dataset successfully created --------------------\n")
+        print("---------- Start loading pre-trained word2vec model ----------")
+        dictionary = load_bin_vec("../Model/GoogleNews-vectors-negative300.bin", vocab)
+        print("---------- Finished loading pre-trained word2vec model ----------\n")
+
+        print("---------- Start saving vectors to file ----------")
+        w, word_idx_map, idx_word_map = get_w(dictionary)
+        pickle.dump([w, word_idx_map, idx_word_map, vocab], open("../Model/dataset.p", "wb"))
+        print("---------- Finished saving vectors to file ----------\n")
+
+        print("-------------------- Dataset successfully created --------------------\n")
+    else:
+        w, word_idx_map, idx_word_map, vocab = pickle.load(open("../Model/dataset.p", "rb"))
+        print("\n---------- File successfully loaded ----------")
+        print("\tLoaded variables: w, word_idx_map, vocab")
+
+    # try:
+    #     print("---------- Start plotting data ----------")
+    #     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    #     plot_only = 500
+    #     low_dim_embs = tsne.fit_transform(w[:plot_only, :])
+    #
+    #     labels = []
+    #     for vec in w[:plot_only]:
+    #         print()
+    #     labels = [idx_word_map[i] for i in w[:plot_only]]
+    #     plot_with_labels(low_dim_embs, labels)
+    #     print("---------- Finished plotting data ----------")
+    # except ImportError:
+    #     print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
+    #     raise
+
