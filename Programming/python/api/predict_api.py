e5f5ff4fd550bd8549f7406eb79eaa8864597f26
naxter <t.moers@gmx.net>
2017-06-28 00:36:35 +0200

    Added CORS support for flask

--- Programming/python/api/predict_api.py
+++ Programming/python/api/predict_api.py
@@ -1,6 +1,7 @@
 import traceback
 
 from flask import Flask, jsonify, request, make_response, abort
+from flask_cors import CORS, cross_origin
 
 from data_processing.emotion_mining.emotion_mining import EmotionMiner
 from data_processing.emotion_mining.negation_handling import NegationHandler
@@ -18,6 +19,7 @@ emotion = EmotionMiner(db)
 sentiment = Sentimenter(db)
 
 app = Flask(__name__)
+cors = CORS(app, supports_credentials=True)
 
 
 @app.route('/predict/single', methods=['POST'])
