f41077d2ed556773d68ec6838a4e3a8296d0e559
Bruno Lubascher <bruno.lubascher@mediaan.com>
2017-06-28 16:37:33 +0200

    changes with linear model

---
+++ Programming/python/data_processing/emotion_mining/load_csv.py
@@ -0,0 +1,24 @@
+from importer.database.data_types import Emotion, Sentence, Comment, Post
+from importer.database.database_access import DataStorage
+from importer.database.mongodb import MongodbStorage
+
+db = MongodbStorage()
+#
+# filter = {Post.COLL_POST_ID: '165836530143824_1281633928564073'}
+# db.select_single_post()
+
+file = 'C:/Users/blu/Dropbox/research_project/Emotions/safeway.csv'
+with open(file, 'r') as file:
+    for line in file.readlines():
+        line = line.rstrip()
+        if len(line) > 1:
+            l = line.split(',')
+            print(l[0])
+
+            filter = {Post.COLL_POST_ID: l[0]}
+            post = db.select_single_post(filter)
+            post.comment_emotion = l[1:]
+
+            print(post.comment_emotion)
+
+            db.update_post(post)
--- Programming/python/neural_networks/data_helpers.py
+++ Programming/python/neural_networks/data_helpers.py
@@ -58,6 +58,52 @@ def get_training_set(db: DataStorage, threshold: int = 1, use_likes: bool = Fals
     return [filtered_posts, new_reactions_matrix]
 
 
+def get_training_set_with_emotions(db: DataStorage, threshold: int = 1, use_likes: bool = False):
+    if use_likes:
+        reactions_right_list = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
+    else:
+        reactions_right_list = ["LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
+
+    data = []
+    reactions_matrix = []
+    emotions = []
+
+    post_filter = {Post.COLL_MESSAGE: {'$exists': True}, Post.COLL_REACTIONS: {'$exists': True},
+                   Post.COLL_COMMENT_EMOTION: {'$exists': True}}
+    for post in db.iterate_single_post(post_filter):
+        data.append(post.message)
+        reactions_matrix.append(post.reactions)
+        emotions.append(post.comment_emotion)
+    [filtered_posts, new_reactions_matrix, new_emotions] = [[], [], []]
+
+    for post, reactions, emots in zip(data, reactions_matrix, emotions):
+        reaction_sum = 0
+        for key in reactions_right_list:
+            reaction_sum += reactions[key]
+
+        if reaction_sum < threshold:
+            continue
+
+        new_reactions = []
+        for key in reactions_right_list:
+            new_reactions.append(reactions[key] / reaction_sum)
+
+        new_emots = []
+        emots = [float(e) for e in emots]
+        emots_sum = sum(emots)
+        if emots_sum > 1:
+            for e in emots:
+                new_emots.append(e/emots_sum)
+        else:
+            new_emots = emots
+
+        filtered_posts.append(post)
+        new_reactions_matrix.append(new_reactions)
+        new_emotions.append(new_emots)
+
+    return [filtered_posts, new_reactions_matrix, new_emotions]
+
+
 def clean_text(text):
     clean_text = []
     for t in text:
