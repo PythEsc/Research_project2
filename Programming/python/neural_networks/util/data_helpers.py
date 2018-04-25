9080a8e10c67b90f6bb070796dcba848aac96d2c
Florian557p <florian557p@hotmail.de>
2018-04-25 18:47:29 +0200

    Code changes: - Adapted Regressor to recent changes

--- Programming/python/neural_networks/util/data_helpers.py
+++ Programming/python/neural_networks/util/data_helpers.py
@@ -154,30 +154,38 @@ def validation_set_iter(db: DataStorage, threshold: int = 1, use_likes: bool = F
             str(count_with_augmented - counter)))
 
 
-def get_training_set_with_emotions(db: DataStorage, threshold: int = 1, use_likes: bool = False):
+def training_set_with_emotions_iter(db: DataStorage, threshold: int = 1, use_likes: bool = False,
+                                    allow_augmented_data: bool = True,
+                                    max_post_length: Optional[int] = None):
     if use_likes:
         reactions_right_list = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
     else:
         reactions_right_list = ["LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
 
-    data = []
-    reactions_matrix = []
-    emotions = []
+    skipped_counter_reactions = 0
+    skipped_counter_length = 0
 
-    post_filter = {Post.COLL_MESSAGE: {'$exists': True}, Post.COLL_REACTIONS: {'$exists': True},
-                   Post.COLL_COMMENT_EMOTION: {'$exists': True}}
+    post_filter = {Post.COLL_MESSAGE: {'$exists': True},
+                   Post.COLL_REACTIONS: {'$exists': True},
+                   Post.COLL_COMMENT_EMOTION: {'$exists': True},
+                   Post.COLL_DATE: {"$gte": datetime(year=2016, month=12, day=31)}}
+
+    count_with_augmented = db.count_posts(filter=post_filter)
+    if not allow_augmented_data:
+        post_filter[Post.COLL_AUGMENTED] = False
+
+    counter = 0
     for post in db.iterate_single_post(post_filter):
-        data.append(post.message)
-        reactions_matrix.append(post.reactions)
-        emotions.append(post.comment_emotion)
-    [filtered_posts, new_reactions_matrix, new_emotions] = [[], [], []]
+        message = post.message
+        reactions = post.reactions
+        emotions = post.comment_emotion
 
-    for post, reactions, emots in zip(data, reactions_matrix, emotions):
         reaction_sum = 0
         for key in reactions_right_list:
             reaction_sum += reactions[key]
 
         if reaction_sum < threshold:
+            skipped_counter_reactions += 1
             continue
 
         new_reactions = []
@@ -185,7 +193,7 @@ def get_training_set_with_emotions(db: DataStorage, threshold: int = 1, use_like
             new_reactions.append(reactions[key] / reaction_sum)
 
         new_emots = []
-        emots = [float(e) for e in emots]
+        emots = [float(e) for e in emotions]
         emots_sum = sum(emots)
         if emots_sum > 1:
             for e in emots:
@@ -193,11 +201,95 @@ def get_training_set_with_emotions(db: DataStorage, threshold: int = 1, use_like
         else:
             new_emots = emots
 
-        filtered_posts.append(post)
-        new_reactions_matrix.append(new_reactions)
-        new_emotions.append(new_emots)
+        message = clean_text(text=[message])[0]
 
-    return [filtered_posts, new_reactions_matrix, new_emotions]
+        if max_post_length is not None:
+            length = len(message.split(" "))
+            if length > max_post_length:
+                skipped_counter_length += 1
+                continue
+
+        counter += 1
+        yield message, new_reactions, new_emots
+
+    if threshold > 0:
+        print("{} posts were skipped because they did not fulfill the reaction-sum requirement".format(
+            str(skipped_counter_reactions)))
+    if max_post_length is not None:
+        print("{} posts were skipped because they did not fulfill the maximum-length requirement".format(
+            str(skipped_counter_length)))
+    if not allow_augmented_data:
+        print("{} posts were skipped because they did not fulfill the not-augmented requirement".format(
+            str(count_with_augmented - counter)))
+
+
+def validation_set_with_emotions_iter(db: DataStorage, threshold: int = 1, use_likes: bool = False,
+                                      allow_augmented_data: bool = True,
+                                      max_post_length: Optional[int] = None):
+    if use_likes:
+        reactions_right_list = ["LIKE", "LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
+    else:
+        reactions_right_list = ["LOVE", "WOW", "HAHA", "SAD", "ANGRY"]
+
+    skipped_counter_reactions = 0
+    skipped_counter_length = 0
+
+    post_filter = {Post.COLL_MESSAGE: {'$exists': True},
+                   Post.COLL_REACTIONS: {'$exists': True},
+                   Post.COLL_COMMENT_EMOTION: {'$exists': True},
+                   Post.COLL_DATE: {"$lt": datetime(year=2016, month=12, day=31)}}
+
+    count_with_augmented = db.count_posts(filter=post_filter)
+    if not allow_augmented_data:
+        post_filter[Post.COLL_AUGMENTED] = False
+
+    counter = 0
+    for post in db.iterate_single_post(post_filter):
+        message = post.message
+        reactions = post.reactions
+        emotions = post.comment_emotion
+
+        reaction_sum = 0
+        for key in reactions_right_list:
+            reaction_sum += reactions[key]
+
+        if reaction_sum < threshold:
+            skipped_counter_reactions += 1
+            continue
+
+        new_reactions = []
+        for key in reactions_right_list:
+            new_reactions.append(reactions[key] / reaction_sum)
+
+        new_emots = []
+        emots = [float(e) for e in emotions]
+        emots_sum = sum(emots)
+        if emots_sum > 1:
+            for e in emots:
+                new_emots.append(e / emots_sum)
+        else:
+            new_emots = emots
+
+        message = clean_text(text=[message])[0]
+
+        if max_post_length is not None:
+            length = len(message.split(" "))
+            if length > max_post_length:
+                skipped_counter_length += 1
+                continue
+
+        counter += 1
+        yield message, new_reactions, new_emots
+
+    if threshold > 0:
+        print("{} posts were skipped because they did not fulfill the reaction-sum requirement".format(
+            str(skipped_counter_reactions)))
+    if max_post_length is not None:
+        print("{} posts were skipped because they did not fulfill the maximum-length requirement".format(
+            str(skipped_counter_length)))
+    if not allow_augmented_data:
+        print("{} posts were skipped because they did not fulfill the not-augmented requirement".format(
+            str(count_with_augmented - counter)))
 
 
 def clean_text(text: list):
--- Programming/python/regression/models.py
+++ Programming/python/regression/models.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+from collections import Iterable
+from random import shuffle
 
 import numpy as np
 from sklearn import metrics
@@ -9,14 +11,17 @@ from data_processing.emotion_mining.negation_handling import NegationHandler
 from importer.database.database_access import DataStorage
 from importer.database.mongodb import MongodbStorage
 from neural_networks.nn_implementations import keras_TextNNBase
-from neural_networks.util.data_helpers import clean_text, get_training_set_with_emotions
+from neural_networks.nn_implementations.keras_TextCNN import TextCNN_Keras
+from neural_networks.util.configuration_loader import DEFAULT_DICT
+from neural_networks.util.data_helpers import training_set_with_emotions_iter, validation_set_with_emotions_iter
 
 
 class Regressor():
-    def __init__(self, db: DataStorage, network: keras_TextNNBase, model='LinearRegression'):
+    def __init__(self, db: DataStorage, network: keras_TextNNBase, settings: dict, model='LinearRegression'):
         self.model = model
         self.db = db
         self.network = network
+        self.settings = settings
 
         if self.model == 'LinearRegression':
             self.regressor = LinearRegression(fit_intercept=True, normalize=True)
@@ -26,47 +31,35 @@ class Regressor():
         self.package_directory = os.path.dirname(os.path.abspath(__file__))
         self.model_path = os.path.join(self.package_directory, 'model.sav')
 
-    def fit(self, sample_percentage: float = 0.1):
-        print('Started training...')
-
-        # Load data
-        print('\nLoading training data...')
-        x_text, y, emotions = get_training_set_with_emotions(self.db)
-        x_text = clean_text(x_text)
-
-        # Randomly Shuffle the data
-        np.random.seed(10)
-        shuffle_indices = np.random.permutation(range(len(y)))
-        x_shuffled = [x_text[i] for i in shuffle_indices]
-        y_shuffled = [y[i] for i in shuffle_indices]
-        emotions_shuffled = [emotions[i] for i in shuffle_indices]
-
-        # Split train/dev set
-        dev_sample_index = -1 * int(sample_percentage * float(len(y)))
-        x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
-        y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
-        emotions_train, emotions_dev = emotions_shuffled[:dev_sample_index], emotions_shuffled[dev_sample_index:]
-        print("\nTrain/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
-
-        # NN prediction
-        print('NN prediction')
-        rnn_out = self.network.predict(x_train)
-        rnn_out_dev = self.network.predict(x_dev)
-
-        output = [np.hstack((rnn_out[i], emotions_train[i])) for i in range(len(x_train))]
-        output_dev = [np.hstack((rnn_out_dev[i], emotions_dev[i])) for i in range(len(x_dev))]
-
-        print('regression')
-        self.regressor.fit(X=output, y=y_train)
-
-        y_pred_n = self.regressor.predict(output_dev)
-
+    def fit(self):
+        # Generate batches
         y_pred = []
-        for i in range(len(y_pred_n)):
-            y_pred.append([(p + 1) / 2 for p in y_pred_n[i]])
+        for train_tuple, valid_tuple in zip(self.training_batch_iterator(batch_size=5000),
+                                            self.validation_batch_iterator(batch_size=500)):
+            x_train = train_tuple[0]
+            y_train = train_tuple[1]
+            emotes_train = train_tuple[2]
 
-        mse = metrics.mean_squared_error(y_dev, y_pred)
-        print('MSE: {}'.format(mse))
+            x_dev = valid_tuple[0]
+            y_dev = valid_tuple[1]
+            emotes_dev = valid_tuple[2]
+
+            nn_out_train = self.network.predict(x=x_train)
+            nn_out_dev = self.network.predict(x=x_dev)
+
+            output = [np.hstack((nn_out_train[i], emotes_train[i])) for i in range(len(x_train))]
+            output_dev = [np.hstack((nn_out_dev[i], emotes_dev[i])) for i in range(len(x_dev))]
+
+            print('regression')
+            self.regressor.fit(X=output, y=y_train)
+
+            y_pred_n = self.regressor.predict(output_dev)
+
+            for i in range(len(y_pred_n)):
+                y_pred.append([(p + 1) / 2 for p in y_pred_n[i]])
+
+            mse = metrics.mean_squared_error(y_dev, y_pred)
+            print('MSE: {}'.format(mse))
 
         pickle.dump(self.regressor, open(self.model_path, 'wb'))
 
@@ -89,10 +82,102 @@ class Regressor():
             y_pred.append([(p + 1) / 2 for p in y_pred_n[i]])
         return y_pred
 
+    def training_batch_iterator(self, batch_size: int, threshold: int = 1, use_likes: bool = False) -> Iterable:
+        x_batch = None
+        y_batch = None
+        emotions_batch = None
+
+        index = 0
+        for index, set_entry in enumerate(
+                training_set_with_emotions_iter(db=self.db, threshold=threshold, use_likes=use_likes,
+                                                max_post_length=self.network.sequence_length,
+                                                allow_augmented_data=self.settings["allow_augmented_data"])):
+
+            x = set_entry[0]
+            y = np.array(set_entry[1], dtype=np.float32)
+            emotions = np.array(set_entry[2], dtype=np.float32)
+
+            x = np.array(list(self.network.vocab_processor.transform([x]))[0], dtype=np.int32)
+
+            if x_batch is None:
+                x_batch = np.empty((batch_size, len(x)), dtype=np.int32)
+            if y_batch is None:
+                y_batch = np.empty((batch_size, len(y)), dtype=np.float32)
+            if emotions_batch is None:
+                emotions_batch = np.empty((batch_size, len(emotions)), dtype=np.float32)
+
+            x_batch[index % batch_size] = x
+            y_batch[index % batch_size] = y
+            emotions_batch[index % batch_size] = emotions
+
+            if index % batch_size == batch_size - 1:
+                x_batch, y_batch, emotions_batch = shuffle(x_batch, y_batch, emotions_batch)
+                yield x_batch, y_batch, emotions_batch
+                x_batch = None
+                y_batch = None
+                emotions_batch = None
+
+        if x_batch is not None and y_batch is not None and emotions_batch is not None:
+            x_batch = x_batch[:(index % batch_size) + 1]
+            y_batch = y_batch[:(index % batch_size) + 1]
+            emotions_batch = emotions_batch[:(index % batch_size) + 1]
+
+            x_batch, y_batch, emotions_batch = shuffle(x_batch, y_batch, emotions_batch)
+            yield x_batch, y_batch, emotions_batch
+
+    def validation_batch_iterator(self, batch_size: int, threshold: int = 1,
+                                  use_likes: bool = False) -> Iterable:
+        x_batch = None
+        y_batch = None
+        emotions_batch = None
+
+        index = 0
+        for index, set_entry in enumerate(
+                validation_set_with_emotions_iter(db=self.db, threshold=threshold, use_likes=use_likes,
+                                                  max_post_length=self.network.sequence_length,
+                                                  allow_augmented_data=self.settings["allow_augmented_data"])):
+
+            x = set_entry[0]
+            y = np.array(set_entry[1], dtype=np.float32)
+            emotions = np.array(set_entry[2], dtype=np.float32)
+
+            x = np.array(list(self.network.vocab_processor.transform([x]))[0], dtype=np.int32)
+
+            if x_batch is None:
+                x_batch = np.empty((batch_size, len(x)), dtype=np.int32)
+            if y_batch is None:
+                y_batch = np.empty((batch_size, len(y)), dtype=np.float32)
+            if emotions_batch is None:
+                emotions_batch = np.empty((batch_size, len(emotions)), dtype=np.float32)
+
+            x_batch[index % batch_size] = x
+            y_batch[index % batch_size] = y
+            emotions_batch[index % batch_size] = emotions
+
+            if index % batch_size == batch_size - 1:
+                x_batch, y_batch, emotions_batch = shuffle(x_batch, y_batch, emotions_batch)
+                yield x_batch, y_batch, emotions_batch
+                x_batch = None
+                y_batch = None
+                emotions_batch = None
+
+        if x_batch is not None and y_batch is not None and emotions_batch is not None:
+            x_batch = x_batch[:(index % batch_size) + 1]
+            y_batch = y_batch[:(index % batch_size) + 1]
+            emotions_batch = emotions_batch[:(index % batch_size) + 1]
+
+            x_batch, y_batch, emotions_batch = shuffle(x_batch, y_batch, emotions_batch)
+            yield x_batch, y_batch, emotions_batch
+
 
 if __name__ == '__main__':
     db = MongodbStorage()
-    reg = Regressor(db)
+    settings = DEFAULT_DICT
+
+    network = TextCNN_Keras(settings=settings)
+    network.load_checkpoint()
+
+    reg = Regressor(db=db, network=network, settings=DEFAULT_DICT)
     reg.fit()
 
     content = [
